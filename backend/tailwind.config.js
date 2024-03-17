06252a36d82692029f5996eb0e21f9330a4efdc8
Hasan <dev.hahuaz@gmail.com>
2024-03-17 05:34:05 +0300

    fix: tailwind can be used by viteexpress

--- backend/tailwind.config.js
+++ backend/tailwind.config.js
@@ -1,10 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 
 console.log("tailwind.config.js __dirname", __dirname);
+console.log("viteexpress using backend/tailwind.config.js");
 export default {
   content: [
-    "./pages/**/*.{html,js,ts,tsx}",
-    "./components/**/*.{html,js,ts,tsx}",
+    "../frontend/pages/**/*.{html,js,ts,tsx}",
+    "../frontend/components/**/*.{html,js,ts,tsx}",
   ],
   theme: {
     extend: {},
